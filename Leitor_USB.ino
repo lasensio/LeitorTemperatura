b38377cfe169c473cb71b70e64e4c8637b539ee1
lasensio <luis.asensio.garcia@gmail.com>
2016-10-17 23:52:29 -0200

    Version with encode Json

--- Leitor_USB.ino
+++ Leitor_USB.ino
@@ -1,3 +1,5 @@
+#include<ArduinoJson.h>
+
 const int LM35 = 0;
 float temperatura = 0;
 int ADClido = 0;
@@ -10,8 +12,19 @@ void setup() {
 void loop() {
   ADClido = analogRead(LM35);
   temperatura = ADClido * 0.1075268817;
-  Serial.print("Temperatura = ");
-  Serial.print(temperatura);
-  Serial.println("*C");
-  delay(1000);
+  //Serial.print("Temperatura = ");
+  //Serial.print(temperatura);
+  //Serial.println("*C");
+  //delay(1000);
+
+  StaticJsonBuffer<200> jsonBuffer;
+
+  JsonObject& root = jsonBuffer.createObject();
+  root["sensorid"] = "8566ee8cc961a20f2f00208063764cfc75082eff5b90f989b36e4da08f935e2a";
+  root["sensor"] = "temperatura";
+  root["temperatura"] = temperatura;
+  root.printTo(Serial);
+  Serial.println();
+
+  delay(2000);
 }
