38c43aa5b3d18819bfe24099099f81df09c810b7
lasensio <luis.asensio.garcia@gmail.com>
2016-10-17 22:59:20 -0200

    Simple version where read temperature via USB

---
+++ Leitor_USB.ino
@@ -0,0 +1,17 @@
+const int LM35 = 0;
+float temperatura = 0;
+int ADClido = 0;
+
+void setup() {
+  Serial.begin(9600);
+  analogReference(INTERNAL);
+}
+
+void loop() {
+  ADClido = analogRead(LM35);
+  temperatura = ADClido * 0.1075268817;
+  Serial.print("Temperatura = ");
+  Serial.print(temperatura);
+  Serial.println("*C");
+  delay(1000);
+}
